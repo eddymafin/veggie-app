6b6447903be318117d57bdb44a711913bdcd5877
eddymafin <marumari2451@gmail.com>
2024-11-30 22:01:19 +0900

    カレンダープラグイン導入

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import jaLocale from "@fullcalendar/core/locales/ja";
+import "../styles/calender.css";
+import { EventContentArg } from "@fullcalendar/core";
 
 function Calender() {
-  return <div></div>;
+  const events = [{ title: "Meeting", start: new Date() }];
+
+  const renderEventContent = (eventInfo: EventContentArg) => {};
+  return (
+    <FullCalendar
+      locale={jaLocale}
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      events={events}
+      eventContent={renderEventContent}
+    ></FullCalendar>
+  );
 }
 
 export default Calender;
