7e27589de634ce26ce3bb1a3fc2132d160447170
eddymafin <marumari2451@gmail.com>
2024-12-01 11:34:18 +0900

    カレンダーにデータを表示

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -5,9 +5,22 @@ import "../styles/calender.css";
 import { EventContentArg } from "@fullcalendar/core";
 
 function Calender() {
-  const events = [{ title: "Meeting", start: new Date() }];
+  // デフォルトのものとカスタムプロパティも追加できる
+  const events = [
+    { title: "Meeting", start: new Date(), veggie: 0, fruit: 0, total: 0 },
+    { title: "aaa", start: "2024-12-05", veggie: 3, fruit: 0, total: 0 },
+  ];
 
-  const renderEventContent = (eventInfo: EventContentArg) => {};
+  const renderEventContent = (eventInfo: EventContentArg) => {
+    console.log(eventInfo);
+    return (
+      <div>
+        <div>veggie:{eventInfo.event.extendedProps.veggie}</div>
+        <div>fruit:{eventInfo.event.extendedProps.fruit}</div>
+        <div>total:{eventInfo.event.extendedProps.total}</div>
+      </div>
+    );
+  };
   return (
     <FullCalendar
       locale={jaLocale}
